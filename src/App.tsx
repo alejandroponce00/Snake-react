c46e1f477afd663b5ea838be10b3cef88ab2e6d7
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-07-06 16:51:26 -0300

    agregado de titulo2

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Game from './game/Game';
+import './App.css';
+
+function App() {<h1>Snake</h1>
+  return <Game />;
+}
+
+export default App;
---
+++ src/game/Game.styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const GameWrapper = styled.div`
+  width: 100%auto;
+  display: flex;
+  align-items: center;
+  outline: none;
+  flex-direction: column;
+`;
+
+export const Score = styled.h1`
+  background: linear-gradient(to left, ##2464F0, #132D68);
+  --webkit-background-clip: text;
+  --webkit-text-fill-color: transparent;
+`;
